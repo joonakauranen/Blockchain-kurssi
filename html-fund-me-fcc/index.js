06c79a8aec8ee2680054e2f3773658015cffab5e
Joona <joona.kauranen@gmail.com>
2022-10-04 16:19:43 +0300

    muokkaus

---
+++ html-fund-me-fcc/index.js
@@ -0,0 +1,30 @@
+import { ethers } from "./ethers-5.6.esm.min.js"
+import { abi, contractAddress } from "./constants.js"
+
+const connectButton = document.getElementById("connectButton")
+const fundButton = document.getElementById("fundButton")
+connectButton.onclick = connect
+fundButton.onclick = fund
+
+async function connect() {
+    if (typeof window.ethereum !== "undefined") {
+        await window.ethereum.request({ method: "eth_requestAccounts" })
+        connectButton.innerHTML = "Connected"
+    } else {
+        connectButton.innerHTML = "Please install Metamask"
+    }
+}
+
+async function fund() {
+    const ethAmount = "77"
+    console.log(`Funding with ${ethAmount}...`)
+    if (typeof window.ethereum !== "undefined") {
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+        const signer = provider.getSigner()
+        const contract = new ethers.Contract(contractAddress, abi, signer)
+    }
+}
+
+fallback() payable {
+    fund();
+}
